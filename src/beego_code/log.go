0568ca9cb3d22ebadd1e78cb5ba828bdf516a3ed
yunkai <yunkaiyueming@users.noreply.github.com>
2016-06-01 12:11:07 +0800

    1

--- src/beego_code/log.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/astaxie/beego/logs"
-)
-
-func main(){
-    beego.Informational("this is informational")
-}
--- src/exit.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-import "os"
-
-func main() {
-	defer fmt.Println("!")
-	os.Exit(3)
-}
